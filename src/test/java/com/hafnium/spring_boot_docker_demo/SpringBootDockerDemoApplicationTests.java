a6b5e89d1859263a801b682e04e5e2aec2a04b42
Hande Fettahoğlu <fettahogluhande@gmail.com>
2024-09-23 12:46:06 +0300

    files added

---
+++ src/test/java/com/hafnium/spring_boot_docker_demo/SpringBootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hafnium.spring_boot_docker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
